bb30cf50c84f8597b7eba1e24e09e995c822fcb6
Sam Olabamiji <samolabams@gmail.com>
2019-10-02 15:15:01 +0100

    Update WelcomeController index response

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,7 +6,7 @@ class WelcomeController extends ApiController
 {
     public function index()
     {
-    	$message = 'Welcome to StarWars API Service. Navigate to ' . '<a href="'.url('api/documentation').'">' . url('api/documentation') . '</a>' . ' to check the docs.';
+    	$message = 'Welcome to StarWars API Service. Navigate to ' . url('api/documentation') . ' to check the docs.';
 
         return $this->respondWithString($message);
     }
